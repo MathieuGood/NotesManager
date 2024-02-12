0070262b9d04d3357611152529f6e379ad448dca
Mathieu <bon.mathieu@gmail.com>
2024-02-12 09:27:08 +0100

    Created project with Maven and started implementing DBConnector class to interact with database.

---
+++ src/main/java/org/serfa/lpdaoo2024/DBConnector.java
@@ -0,0 +1,191 @@
+package org.serfa.lpdaoo2024;
+
+import java.sql.*;
+
+public abstract class DBConnector {
+
+    // Credentials for database connection
+    private static Connection connection;
+    final static private String dbHost = "51.91.98.35";
+    final static private String dbPort = "3306";
+    final static private String dbUsername = "notesmanager";
+    final static private String dbPassword = "notesserfa2024";
+    final static private String dbName = "NotesManager";
+
+
+    // Get all users from database
+    public static void getUsers() {
+
+        try {
+            openDatabaseConnection();
+
+            System.out.println("Getting all users");
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT *
+                    FROM users
+                    """);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String name = resultSet.getString(2);
+                String email = resultSet.getString(3);
+                System.out.println("\t> " + name + " / " + email);
+            }
+
+            closeDatabaseConnection();
+
+        } catch (SQLException e) {
+            System.out.println("SQL Error : " + e);
+        }
+    }
+
+
+    // Check if user email and password match
+    public static boolean checkPasswordMatch(String email, String password) {
+
+        try {
+            openDatabaseConnection();
+
+            System.out.println("Checking if user e-mail and password match");
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT COUNT(*)
+                    FROM users
+                    WHERE user_email = ? AND user_password = ?
+                    """);
+
+            statement.setString(1, email);
+            statement.setString(2, password);
+
+            ResultSet resultSet = statement.executeQuery();
+            boolean match = false;
+
+            resultSet.next();
+
+            if (resultSet.getInt(1) == 1) {
+                match = true;
+                System.out.println("Match found");
+            }
+
+            closeDatabaseConnection();
+            return match;
+
+        } catch (SQLException e) {
+            System.out.println("SQL Error : " + e);
+        }
+        return false;
+    }
+
+
+    // Create new user in database provided name, email and password
+    // TO DO :
+    // Add a check to see if the user already exists
+    public static void createUser(String name, String email, String password) {
+
+        try {
+
+            openDatabaseConnection();
+
+            System.out.println("Inserting : " + name + " / " + email + " / " + password);
+
+            PreparedStatement statement = connection.prepareStatement("""
+                        INSERT INTO users (user_name, user_email, user_password)
+                        VALUES (?, ?, ?);
+                    """);
+
+            statement.setString(1, name);
+            statement.setString(2, email);
+            statement.setString(3, password);
+
+            int rowsInserted = statement.executeUpdate();
+            System.out.println("Number of rows inserted : " + rowsInserted);
+
+            closeDatabaseConnection();
+
+        } catch (SQLException e) {
+            System.out.println("SQL Error : " + e);
+        }
+    }
+
+
+    // Updated user password based on provided user_email
+    public static void updateUserPassword(String email, String newPassword) {
+
+        try {
+            openDatabaseConnection();
+
+            System.out.println("Updating password for " + email);
+
+            PreparedStatement statement = connection.prepareStatement("""
+                    UPDATE users
+                    SET user_password = ?
+                    WHERE user_email = ?
+                    """);
+            statement.setString(1, newPassword);
+            statement.setString(2, email);
+            int rowsUpdated = statement.executeUpdate();
+            System.out.println("Row updated : " + rowsUpdated);
+
+            closeDatabaseConnection();
+        } catch (SQLException e) {
+            System.out.println("SQL Error : " + e);
+        }
+    }
+
+
+    // Updated user name based on provided user_email
+    public static void updateUserName(String email, String newName) {
+
+        try {
+            openDatabaseConnection();
+
+            System.out.println("Updating username for " + email);
+
+            PreparedStatement statement = connection.prepareStatement("""
+                    UPDATE users
+                    SET user_name = ?
+                    WHERE user_email = ?
+                    """);
+            statement.setString(1, newName);
+            statement.setString(2, email);
+            int rowsUpdated = statement.executeUpdate();
+            System.out.println("Row updated : " + rowsUpdated);
+
+            closeDatabaseConnection();
+        } catch (SQLException e) {
+            System.out.println("SQL Error : " + e);
+        }
+    }
+
+
+    // Delete user based on provided user_email
+    public static void deleteUser(String email) {
+
+        try {
+            openDatabaseConnection();
+
+            PreparedStatement statement = connection.prepareStatement("""
+                    DELETE FROM users WHERE user_email = ?
+                    """);
+            System.out.println("Deleting " + email);
+            statement.setString(1, email);
+            statement.executeUpdate();
+
+            closeDatabaseConnection();
+        } catch (SQLException e) {
+            System.out.println("SQL Error : " + e);
+        }
+    }
+
+    // Open database connection
+    private static void openDatabaseConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mariadb://" + dbHost + ":" + dbPort + "/" + dbName, dbUsername, dbPassword);
+        System.out.println("Connection validity to " + dbName + " : " + connection.isValid(5));
+    }
+
+    // Close database connection
+    private static void closeDatabaseConnection() throws SQLException {
+        System.out.println("Closing connection to " + dbName);
+        connection.close();
+    }
+
+
+}
---
+++ src/main/java/org/serfa/lpdaoo2024/Main.java
@@ -0,0 +1,28 @@
+package org.serfa.lpdaoo2024;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        runDatabaseOperations();
+
+    }
+
+    public static void runDatabaseOperations() {
+
+        // Create new user
+        // DBConnector.createUser("Mat", "mathieu@mathieu.com", "123456");
+
+        // Update user password
+        // DBConnector.updateUserPassword("mathieu@mathieu.com", "newpassword");
+
+        // Update user name
+        // DBConnector.updateUserName("mathieu@mathieu.com", "NouveauMathieu");
+
+        // Check if username and password match
+        // DBConnector.checkPasswordMatch("mathieu@mathieu.com", "newpassword");
+
+        // Delete user
+        // DBConnector.deleteUser("mathieu@mathieu.com");
+    }
+}
