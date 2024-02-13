f1edae45fa9b21cb1b28acfc2ef72bbe9f200f7f
Mathieu <bon.mathieu@gmail.com>
2024-02-13 11:22:47 +0100

    Updated Main to launch LoginWindow at start. Set FlatLaf as default IUManager.

--- src/main/java/org/serfa/lpdaoo2024/Main.java
+++ src/main/java/org/serfa/lpdaoo2024/Main.java
@@ -1,18 +1,29 @@
 package org.serfa.lpdaoo2024;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        runDatabaseOperations();
+        // Set FlatLaf "Look and Feel" for Swing
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
+        }
+
+        // Open Login Window
+        LoginWindow window = new LoginWindow();
 
     }
 
     public static void runDatabaseOperations() {
 
         // Create new user
-        System.out.println(DBConnector.createUser("Mat", "mathieu@test.com", "123456"));
-        System.out.println(DBConnector.createUser("Mat", "test@test.com", "123456"));
+//        System.out.println(DBConnector.createUser("Mat", "mathieu@test.com", "123456"));
 
         // Update user password
         // DBConnector.updateUserPassword("mathieu@mathieu.com", "newpassword");
