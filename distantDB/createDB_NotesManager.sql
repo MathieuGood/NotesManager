451f11803bdf58f8be53df1c90aa6d8dd4e8f678
Mathieu <bon.mathieu@gmail.com>
2024-02-20 16:31:29 +0100

    Added hex color codes field to colors tables.

--- distantDB/createDB_NotesManager.sql
+++ distantDB/createDB_NotesManager.sql
@@ -30,54 +30,58 @@ CREATE TABLE users(
    user_email VARCHAR(50),
    user_password VARCHAR(50),
    PRIMARY KEY(user_id),
-   INDEX(user_email),
-   ADD CONSTRAINT UC_user_email UNIQUE (user_email)
+   INDEX(user_email)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+ALTER TABLE users ADD CONSTRAINT UC_user_email UNIQUE (user_email);
 
 INSERT INTO users(
    user_name,
    user_email,
    user_password
 ) VALUES
-   ('Mathieu', 'bon.mathieu@gmail.com', 'mathieubon'),
-   ('Soundouce', 'soundouce.chibani@gmail.com', 'soundouce'),
-   ('Youssef', 'you.moudni@gmail.com', 'youssef'),
-   ('Belgrand', 'grmabele@gmail.com', 'belgrand'),
-   ('Testeur', 'test@test.com', 'test');
+   ('Mathieu', 'bon.mathieu@gmail.com', 'Mathieu*1'),
+   ('Soundouce', 'soundouce.chibani@gmail.com', 'Soundouce*1'),
+   ('Youssef', 'you.moudni@gmail.com', 'Youssef*1'),
+   ('Belgrand', 'grmabele@gmail.com', 'Belgrand*1'),
+   ('Testeur', 'test@test.com', 'Testeur*1'),
+   ('a', 'a@a.com', 'Aaaaaa*1');
 
 
 CREATE TABLE colors(
    color_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
    color_name VARCHAR(50),
+   color_hex VARCHAR(7),
    PRIMARY KEY(color_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 INSERT INTO colors(
-   color_name
+   color_name,
+   color_hex
 ) VALUES
-   ('red'),
-   ('blue'),
-   ('green'),
-   ('yellow'),
-   ('orange'),
-   ('purple'),
-   ('pink'),
-   ('brown'),
-   ('black'),
-   ('white');
-
+   ('red', '#FF0000'),
+   ('blue', '#0000FF'),
+   ('green', '#008000'),
+   ('yellow', '#FFFF00'),
+   ('orange', '#FFA500'),
+   ('purple', '#800080'),
+   ('pink', '#FFC0CB'),
+   ('brown', '#A52A2A'),
+   ('black', '#000000'),
+   ('white', '#FFFFFF');
+   
 
 CREATE TABLE labels(
    label_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
    label_name VARCHAR(50),
-   color_id INT UNSIGNED NOT NULL,
+   label_color_id INT UNSIGNED NOT NULL,
    PRIMARY KEY(label_id),
-   FOREIGN KEY(color_id) REFERENCES colors(color_id)
+   FOREIGN KEY(label_color_id) REFERENCES colors(color_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 INSERT INTO labels(
    label_name,
-   color_id
+   label_color_id
 ) VALUES
    ('Important', 1),
    ('Urgent', 2),
@@ -94,16 +98,16 @@ CREATE TABLE binders(
    binder_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
    binder_name VARCHAR(50),
    user_id INT UNSIGNED NOT NULL,
-   color_id INT UNSIGNED NOT NULL,
+   binder_color_id INT UNSIGNED,
    PRIMARY KEY(binder_id),
    FOREIGN KEY(user_id) REFERENCES users(user_id),
-   FOREIGN KEY(color_id) REFERENCES colors(color_id)
+   FOREIGN KEY(binder_color_id) REFERENCES colors(color_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 INSERT INTO binders(
    binder_name,
    user_id,
-   color_id
+   binder_color_id
 ) VALUES
    ('Cours SERFA', 1, 2),
    ('My First Binder', 2, 1),
@@ -116,16 +120,16 @@ CREATE TABLE tabs(
    tab_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
    tab_name VARCHAR(50),
    binder_id INT UNSIGNED NOT NULL,
-   color_id INT UNSIGNED NOT NULL,
+   tab_color_id INT UNSIGNED,
    PRIMARY KEY(tab_id),
    FOREIGN KEY(binder_id) REFERENCES binders(binder_id),
-   FOREIGN KEY(color_id) REFERENCES colors(color_id)
+   FOREIGN KEY(tab_color_id) REFERENCES colors(color_id)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 INSERT INTO tabs(
    tab_name,
    binder_id,
-   color_id
+   tab_color_id
 ) VALUES
    ('Java', 1, 2),
    ('React Native', 1, 3),
@@ -139,6 +143,7 @@ INSERT INTO tabs(
 CREATE TABLE notes(
    note_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
    note_name VARCHAR(50),
+   note_color_id INT UNSIGNED,
    note_content TEXT,
    tab_id INT UNSIGNED NOT NULL,
    PRIMARY KEY(note_id),
@@ -147,17 +152,18 @@ CREATE TABLE notes(
 
 INSERT INTO notes(
    note_name,
+   note_color_id,
    note_content,
    tab_id
 ) VALUES
-   ('Syntax', 'Print a statement : System.out.println("This is a statement")', 1),
-   ('Create a class', 'Do not forget to add the constructor.', 1),
-   ('React Native Introduction', 'React Native is a framework for building native apps using React.', 2),
-   ('SQL Introduction', 'SQL is a standard language for storing, manipulating and retrieving data in databases.', 3),
-   ('My First Note', 'This is my first note.', 4),
-   ('My First Note', 'This is my first note.', 5),
-   ('My First Note', 'This is my first note.', 6),
-   ('My First Note', 'This is my first note.', 7);
+   ('Syntax', NULL,  'Print a statement : System.out.println("This is a statement")', 1),
+   ('Create a class', 2, 'Do not forget to add the constructor.', 1),
+   ('React Native Introduction', 4,'React Native is a framework for building native apps using React.', 2),
+   ('SQL Introduction', NULL,'SQL is a standard language for storing, manipulating and retrieving data in databases.', 3),
+   ('My First Note', NULL, 'This is my first note.', 4),
+   ('My First Note', NULL, 'This is my first note.', 5),
+   ('My First Note', NULL, 'This is my first note.', 6),
+   ('My First Note', NULL, 'This is my first note.', 7);
 
 
 CREATE TABLE tag(
---
+++ src/main/java/org/serfa/lpdaoo2024/DatabaseManager.java
@@ -0,0 +1,46 @@
+package org.serfa.lpdaoo2024;
+
+import java.sql.*;
+
+public abstract class DBConnector {
+
+    // Credentials for database connection
+    // TO DO : move credentials to a separate file for security
+    final static private String dbHost = "51.91.98.35";
+    final static private String dbPort = "3306";
+    final static private String dbUsername = "notesmanager";
+    final static private String dbPassword = "notesserfa2024";
+    final static private String dbName = "NotesManager";
+
+
+    /**
+     * This method opens a connection to the database using the provided database credentials.
+     * It uses the DriverManager to get a connection to the MariaDB database at the specified host and port.
+     * The method then checks the validity of the connection by calling the isValid method on the connection object.
+     * If the connection is valid, it prints a message indicating the validity and the name of the database.
+     * The method then returns the connection object.
+     *
+     * @return A Connection object representing the open database connection.
+     * @throws SQLException If a database access error occurs.
+     */
+    public static Connection openDatabaseConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://" + dbHost + ":" + dbPort + "/" + dbName, dbUsername, dbPassword);
+        System.out.println("Connection validity to " + dbName + " : " + connection.isValid(5));
+        return connection;
+    }
+
+
+    /**
+     * This method closes the connection to the database.
+     * It prints a message indicating that the connection to the database is being closed.
+     * Then, it calls the close method on the provided Connection object.
+     *
+     * @param connection The Connection object representing the open database connection.
+     * @throws SQLException If a database access error occurs.
+     */
+    public static void closeDatabaseConnection(Connection connection) throws SQLException {
+        System.out.println("Closing connection to " + dbName);
+        connection.close();
+    }
+
+}
