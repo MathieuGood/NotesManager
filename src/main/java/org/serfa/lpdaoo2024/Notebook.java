11fcaca9204990cd5f9ef2c49a079672ed6f8106
Mathieu <bon.mathieu@gmail.com>
2024-02-26 19:36:52 +0100

    Completed Notebook class so it can get all binders, create new binder and delete existing binder.

--- src/main/java/org/serfa/lpdaoo2024/Notebook.java
+++ src/main/java/org/serfa/lpdaoo2024/Notebook.java
@@ -15,39 +15,33 @@ public class Notebook {
         return userID;
     }
 
+
     public void getContentTree() {
 
+        System.out.println("\n***");
+        System.out.println("getContentTree() for userID " + userID);
+
+        ResultSet resultSet = DatabaseManager.select(
+                "binders "
+                        + "INNER JOIN tabs ON binders.binder_id = tabs.binder_id "
+                        + "INNER JOIN notes ON tabs.tab_id = notes.tab_id",
+                new String[]{
+                        "binders.binder_id",
+                        "binders.binder_name",
+                        "binders.binder_color_id",
+                        "tabs.tab_id",
+                        "tabs.tab_name",
+                        "tabs.tab_color_id",
+                        "notes.note_id",
+                        "notes.note_name",
+                        "notes.note_color_id"
+                },
+                "user_id",
+                String.valueOf(userID));
+
+        // Print ResultSet data
         try {
-            Connection connection = DatabaseManager.openDatabaseConnection();
-            System.out.println("\n***");
-            System.out.println("getContentTree() for userID " + userID);
-
-            PreparedStatement statement = connection.prepareStatement("""
-                                SELECT
-                                	binders.binder_id,
-                                    binders.binder_name,
-                                    binders.binder_color_id,
-                                    tabs.tab_id,
-                                    tabs.tab_name,
-                                    tabs.tab_color_id,
-                                    notes.note_id,
-                                    notes.note_name,
-                                    notes.note_color_id
-                                    FROM binders
-                                        INNER JOIN tabs
-                                            ON binders.binder_id = tabs.binder_id
-                                        INNER JOIN notes
-                                            ON tabs.tab_id = notes.tab_id
-                                
-                                WHERE user_id = ?;
-                            """,
-                    Statement.RETURN_GENERATED_KEYS);
-
-            statement.setInt(1, userID);
-
-            ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                // Print out data from resultSet
                 int binderID = resultSet.getInt(1);
                 String binderName = resultSet.getString(2);
                 int binderColorID = resultSet.getInt(3);
@@ -59,72 +53,48 @@ public class Notebook {
                 int noteColorID = resultSet.getInt(9);
                 System.out.println("\t> " + binderID + " / " + binderName + " / " + binderColorID + " / " + tabID + " / " + tabName + " / " + tabColorID + " / " + noteID + " / " + noteName + " / " + noteColorID);
             }
-
-            DatabaseManager.closeDatabaseConnection(connection);
-
-        } catch (SQLIntegrityConstraintViolationException e) {
-            // Entry already exists
-            System.out.println("SQL Integrity Constraint Violation : " + e);
-
-        } catch (SQLException e) {
-            // Other error
-            System.out.println("SQL Error : " + e);
-
+        } catch (Exception e) {
+            System.out.println("Error : " + e);
         }
     }
 
 
     public ArrayList<Binder> getAllBinders() {
+        System.out.println("\n***");
+        System.out.println("getAllBinders() for userID " + this.userID + " :");
 
+        ResultSet resultSet = DatabaseManager.select(
+                "binders",
+                new String[]{
+                        "binders.binder_id",
+                        "binders.binder_name",
+                        "binders.binder_color_id"
+                },
+                "user_id",
+                String.valueOf(userID));
+
+        // Create ArrayList to store all Binder objects
+        ArrayList<Binder> binders = new ArrayList<>();
+
+        // Parse query results to new Binder object and store it into ArrayList
         try {
-            Connection connection = DatabaseManager.openDatabaseConnection();
-            System.out.println("\n***");
-            System.out.println("getAllBinders() for userID " + this.userID + " :");
-
-            PreparedStatement statement = connection.prepareStatement("""
-                                SELECT
-                                	binders.binder_id,
-                                    binders.binder_name,
-                                    binders.binder_color_id
-                                FROM binders
-                                WHERE user_id = ?;
-                            """,
-                    Statement.RETURN_GENERATED_KEYS);
-
-            // Add userID to query
-            statement.setInt(1, userID);
-
-            // Execute query and store results in ResultSet
-            ResultSet resultSet = statement.executeQuery();
-
-
-            // Create ArrayList to store all Binder objects
-            ArrayList<Binder> binders = new ArrayList<>();
-
-            // Parse query results to new Binder object and store it into ArrayList
             while (resultSet.next()) {
-                // Print out data from resultSet
+                // Retrieve data from resultSet
                 int binderID = resultSet.getInt(1);
                 String binderName = resultSet.getString(2);
                 int binderColorID = resultSet.getInt(3);
+
+                // Print out data from resultSet
                 System.out.println("\t> " + binderID + " / " + binderName + " / " + binderColorID);
+
+                // Create new Binder object with parsed data and add it to ArrayList
                 binders.add(new Binder(this, binderID, binderName, binderColorID));
             }
-
-            DatabaseManager.closeDatabaseConnection(connection);
-
-            return binders;
-
-        } catch (SQLIntegrityConstraintViolationException e) {
-            // Entry already exists
-            System.out.println("SQL Integrity Constraint Violation : " + e);
-
-        } catch (SQLException e) {
-            // Other error
-            System.out.println("SQL Error : " + e);
-
+        } catch (Exception e) {
+            System.out.println("Error : " + e);
+            return null;
         }
-        return null;
+        return binders;
     }
 
 
@@ -140,44 +110,10 @@ public class Notebook {
 
 
     public int deleteBinder(int binderID) {
+        System.out.println("\n***");
+        System.out.println("deleteBinder() : " + " binderID " + binderID);
 
-        try {
-            System.out.println("\n***");
-            System.out.println("deleteBinder() : " + " binderID " + binderID);
-
-            Connection connection = DatabaseManager.openDatabaseConnection();
-
-            // Delete binder and all associated children (tabs and notes) from database
-            // userID is used to prevent deletion of other user's data
-
-            PreparedStatement statement = connection.prepareStatement("""
-                        DELETE FROM binders
-                        WHERE binder_id = ?
-                        AND user_id = ?;
-                        ;
-                    """);
-
-            statement.setInt(1, binderID);
-            statement.setInt(2, userID);
-
-            // Execute the statement
-            int rowsDeleted = statement.executeUpdate();
-            System.out.println("Rows deleted : " + rowsDeleted);
-
-            DatabaseManager.closeDatabaseConnection(connection);
-            return rowsDeleted;
-
-        } catch (SQLIntegrityConstraintViolationException e) {
-            // Cannot delete because of foreign key constraint
-            System.out.println("SQL Integrity Constraint Violation : " + e);
-            return 0;
-
-        } catch (SQLException e) {
-            // Other error
-            System.out.println("SQL Error : " + e);
-            return -1;
-        }
-
+        return DatabaseManager.delete("binders", "binderID", String.valueOf(binderID));
     }
 
 
